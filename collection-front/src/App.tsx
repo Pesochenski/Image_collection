536d59a07c22d4f0a1e8a081110d67261fd2ffc3
Pesochenski <pesochenski.corp@gmail.com>
2021-06-13 05:18:26 +0300

    build (#1)

--- collection-front/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.scss";
-import Header from "./components/header/index";
-
-function App(): JSX.Element {
-  return <Header />;
-}
-
-export default App;
--- collection-front/src/interfaces/headerInterfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface headerItem {
-  name: string;
-  path: string;
-  burger: {
-    from: number;
-    to: number;
-  };
-}
